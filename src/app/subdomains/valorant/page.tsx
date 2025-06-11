e505cfe2dfa8a0ced51ef8a1c342c2561f8b74a2
Lawlzer <kevindaspam@gmail.com>
2025-06-10 22:40:36 -0500

    Valorant UI: Remove topbar clutter and fix map display height
    - Removed map name and click instructions from topbar
    
    - Fixed map area not extending to bottom
    
    - Re-added zoom controls in better position

--- src/app/subdomains/valorant/page.tsx
+++ src/app/subdomains/valorant/page.tsx
@@ -1,13 +1,27 @@
 'use client';
 
 import type { JSX } from 'react';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import ValorantLineupClient from './components/ValorantLineupClient';
 
 export default function ValorantPage(): JSX.Element {
+	// Hide skip links on this page by adding a class that CSS can target
+	useEffect(() => {
+		const skipLinks = document.querySelectorAll('a[href="#main-content"], a[href="#navigation"]');
+		skipLinks.forEach((link) => {
+			link.classList.add('!hidden');
+		});
+
+		return () => {
+			skipLinks.forEach((link) => {
+				link.classList.remove('!hidden');
+			});
+		};
+	}, []);
+
 	return (
-		<div className='flex h-screen w-full flex-col'>
+		<div className='h-[calc(100vh-4rem)] w-full overflow-hidden' style={{ touchAction: 'manipulation' }}>
 			<ValorantLineupClient />
 		</div>
 	);
